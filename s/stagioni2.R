db758a23f7e7cee7cbca87400507d18637146851
PolaroidAndroid <MOROSTEFANO87@GMAIL.COM>
2017-12-08 10:18:05 -0800

    PCA --- vai avanti tu!

--- s/stagioni2.R
+++ s/stagioni2.R
@@ -1,9 +1,11 @@
+#setto la WD ----
 pm10<-read.csv2("data/pm2010.csv",dec=".")
 
-
+#questo è un pacchetto di pacchetti. Contiene molti pacchetti utili :-)
 install.packages("tidyverse")
 
 library(tidyr)
+#crea variabile per anno, giorno, mese
 pm10_a <- separate(pm10, col="data", into = c("anno","mese","giorno"), sep="-")
 
 pm10_a$mese2 <- as.numeric(pm10_a$mese)
@@ -18,7 +20,7 @@ str(pm10_a)
 # stagioni: primavera (21-03 / 20-06), estate (21-06/22-09), autunno (23-09 /21-12)
 # inverno(22-12/20-03)
 
-
+#divido in stagioni
 getSeason <- function(DATES) {
   WS <- as.Date("2010-12-22", format = "%Y-%m-%d") # Winter Solstice
   SE <- as.Date("2010-3-21",  format = "%Y-%m-%d") # Spring Equinox
@@ -42,16 +44,59 @@ table(pm10_a$Stagione)
 
 
 summary(pm10_a)
+
+### PCA ANALYSIS ----
 require(ade4)
-pca1<-dudi.pca(deug$tab,scal=FALSE,center=deug$cent,scan=FALSE,nf=2)
-pcapm<-dudi.pca(pm10_a[,-10],scal=FALSE,center=TRUE,scan=FALSE,nf=2)
-scatter(pcapm$eig)
+names(pm10_a)
+
+#estraggo da pm10 solo le variabili numeriche da inserire nella PCA escludendo giorno mese e anno che sono da considerarsi factors
+data_pca <- pm10_a[,c(1:9)]
+
+# con nf = 3 gli stò dicendo di tenere tre assi
+pcapm<-dudi.pca(data_pca,scannf = FALSE, nf = 3)
+
+#faccio il biplot
+scatter(pcapm, clab.row = 0.5)
+
+#valori dei punteggi per le variabili
+pcapm$c1
+
+# CARATTERIZZIAMO GLI ASSI
+# se vedi come ci si aspetta mediana media e max stanno insieme perchè sono molto correlati e vanno tutti sulla prima componente
+# sulla seconda ci vanno i fattori ambientali tmp, umr, rdz, pgg e prs. Solo che tmp, prs e rdz hanno correlazione negativa rispetto
+# a umr  e pgg [pgg è al limite. ha un punteggio abbastanza basso]
+# sulla terza si prende tutto il vento
+
+#cerchio di correlazione, i punteggi delle varibili sono plottati su una circonferenza di raggio unitario
+s.corcircle(pcapm$c1, xax = 1, yax = 2) #plotto prima e seconda
+s.corcircle(pcapm$c1, xax = 1, yax = 3) #plotto prima e terza
+s.corcircle(pcapm$c1, xax = 2, yax = 3) #plotto seconda e terza
+
+#calcolo gli autovalori
+pcapm$eig/sum(pcapm$eig) 
+
+#calcolo la somma cumilata degli autovalori
+cumsum(pcapm$eig/sum(pcapm$eig)) # con i primi tre assi spiego circa il 70% che non è male
+
+#vediamo adesso come si comportano le variabili qualitative - lascio a te l'interpretazione!!!!!!
+# STAGIONE
+s.class(pcapm$li, factor(pm10_a$Stagione), xax = 1, yax = 2)
+s.class(pcapm$li, factor(pm10_a$Stagione), xax = 1, yax = 3)
+s.class(pcapm$li, factor(pm10_a$Stagione), xax = 2, yax = 3)
+
+#MESE
+s.class(pcapm$li, factor(pm10_a$mese), xax = 1, yax = 2)
+s.class(pcapm$li, factor(pm10_a$mese), xax = 1, yax = 3)
+s.class(pcapm$li, factor(pm10_a$mese), xax = 2, yax = 3)
+
+# PROVA A FARLO ANCHE CON IL GIORNO E LA DIREZIONE DEL VENTO E PROVA A METTERE COLORI DIVERSI
 
 
 str(pm10_a)
 pm10_a$giorno<-as.factor(pm10_a$giorno)
 pm10_a$mese<-as.factor(pm10_a$mese)
 pm10_a$anno<-as.factor(pm10_a$anno)
+pm10_a$Stagione<-as.factor(pm10_a$Stagione)
 
 
 dd1 <- dudi.hillsmith(pm10_a, scann = FALSE)
